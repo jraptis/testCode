573756f107bf80f86477ee13ed0f5bc992b86c22
Ioannis Raptis <raptis.y@gmail.com>
2024-09-17 13:02:53 +0300

    Update WebPortalDevController.php

--- WebPortalDevController.php
+++ WebPortalDevController.php
@@ -1,10 +1,6 @@
 <?php
-
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
 class WebPortalDevController extends Controller
 {    
    
@@ -47,41 +43,6 @@ class WebPortalDevController extends Controller
             $path = base_path() . "/storage/pdfreportfiles/chart-events-" . $data["data"]["vat"] . ".png";
             file_put_contents($path, $response);
             return $path;
-        } elseif ($type == "pm1") {
-            $r = array();
-            $r["chart_data"] = array();
-
-            if (isset($data["data"]["money"]["decisionByYear"]["awards"])) {
-                $r["chart_data"]["Awards"]["data"] = $data["data"]["money"]["decisionByYear"]["awards"];
-            } else {
-                $r["chart_data"]["Awards"] = null;
-            }
-            if (isset($data["data"]["money"]["decisionByYear"]["assignments"])) {
-                $r["chart_data"]["Assignments"]["data"] = $data["data"]["money"]["decisionByYear"]["assignments"];
-            } else {
-                $r["chart_data"]["Assignments"] = null;
-            }
-            if (isset($data["data"]["money"]["decisionByYear"]["payments"])) {
-                $r["chart_data"]["Payments"]["data"] = $data["data"]["money"]["decisionByYear"]["payments"];
-            } else {
-                $r["chart_data"]["Payments"] = null;
-            }
-            if (isset($data["data"]["money"]["decisionByYear"]["approvals"])) {
-                $r["chart_data"]["Approvals"]["data"] = $data["data"]["money"]["decisionByYear"]["approvals"];
-            } else {
-                $r["chart_data"]["Approvals"] = null;
-            }
-            $jdata = str_replace("amount", "value", json_encode($r));
-
-            $endpoint = config("keycloak.BRcreatechart") . '?chart_type=per_year&lang='.$lang;
-            $curl = $this->preparePostCurl($endpoint, $token, $jdata, 120);
-            $execute = $this->ExecuteCurl($curl);
-            $response = $execute[0];
-
-            $path = base_path() . "/storage/pdfreportfiles/pm1-" . $data["data"]["vat"] . ".png";
-            file_put_contents($path, $response);
-            return $path;
         } 
     }
-
 }
