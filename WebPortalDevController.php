4f8e29a3ce04c1a18ba73a611966636972669786
Ioannis Raptis <raptis.y@gmail.com>
2024-09-17 13:01:44 +0300

    Update WebPortalDevController.php

--- WebPortalDevController.php
+++ WebPortalDevController.php
@@ -81,55 +81,7 @@ class WebPortalDevController extends Controller
             $path = base_path() . "/storage/pdfreportfiles/pm1-" . $data["data"]["vat"] . ".png";
             file_put_contents($path, $response);
             return $path;
-        } elseif ($type == "pm2") {
-            $r = array();
-            $r["chart_data"] = array();
-            $r["chart_data"]["Payments"] = array();
-            $r["chart_data"]["Payments"]["perBuyer"] = array();
-
-            if (isset($data["data"]["money"]["largerPayers"])) {
-                foreach ($data["data"]["money"]["largerPayers"] as $k => $v) {
-                    if ($v["percentage"] != 0 && $v["percentage"] != null) {
-                        array_push($r["chart_data"]["Payments"]["perBuyer"], $v);
-                    }
-                }
-            }
-           
-            $jdata = json_encode($r);
-            $endpoint = config("keycloak.BRcreatechart") . '?chart_type=top3_buyers&lang='.$lang;
-            $curl = $this->preparePostCurl($endpoint, $token, $jdata, 120);
-            $execute = $this->ExecuteCurl($curl);
-            $response = $execute[0];
-
-            $path = base_path() . "/storage/pdfreportfiles/pm2-" . $data["data"]["vat"] . ".png";
-            file_put_contents($path, $response);
-            return $path;
-        } elseif ($type == "unfav") {
-            $r = array();
-            $r["chart_data"] = array();
-
-            if (isset($data["data"]["unfavorable"]["transactional"]["overall"])) {
-                $r["chart_data"]["overall"] = $data["data"]["unfavorable"]["transactional"]["overall"];
-            }
-            if (isset($data["data"]["unfavorable"]["transactional"]["per_years_list"])) {
-                $r["chart_data"]["per_year"] = array();
-                foreach ($data["data"]["unfavorable"]["transactional"]["per_years_list"] as $k => $v) {
-                    $r["chart_data"]["per_year"][$v["year"]]["score"] = $v["score"];
-                    $r["chart_data"]["per_year"][$v["year"]]["index"] = $v["ranking"];
-                }
-            }
-            $jdata = json_encode($r);
-            $jdata = str_replace("rating", "index", $jdata);
-
-            $endpoint = config("keycloak.BRcreatechart") . '?chart_type=unfavorable&lang='.$lang;
-            $curl = $this->preparePostCurl($endpoint, $token, $jdata, 120);
-            $execute = $this->ExecuteCurl($curl);
-            $response = $execute[0];
-           
-            $path = base_path() . "/storage/pdfreportfiles/unfav-" . $data["data"]["vat"] . ".png";
-            file_put_contents($path, $response);
-            return $path;
-        }
+        } 
     }
 
 }
